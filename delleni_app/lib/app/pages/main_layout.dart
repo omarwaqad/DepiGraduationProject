cc5b710a75a583f24614adb08d5e826b0bed092c
omarwaqqad <waqado@gmail.com>
2025-12-12 06:09:40 +0200

    .

--- delleni_app/lib/app/pages/main_layout.dart
+++ delleni_app/lib/app/pages/main_layout.dart
@@ -1,4 +1,5 @@
 // lib/app/pages/main_layout.dart
+import 'package:delleni_app/app/pages/locations.dart';
 import 'package:delleni_app/app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -20,10 +21,7 @@ class _MainLayoutState extends State<MainLayout> {
   final List<Widget> _pages = [
     HomeContent(), // Just the content part without navigation
     UserProgressScreen(),
-    Container(
-      // Placeholder for Places page
-      child: Center(child: Text('الأماكن - قيد التطوير')),
-    ),
+    LocationsPage(),
     SocietyPage(),
     ProfilePage(),
   ];
